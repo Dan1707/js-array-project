8adaaed1dd9442ceddcb0923f6adaa6f799b2b32
Dan1707 <danuhae@gmail.com>
2023-09-19 18:36:43 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+////////////////////////// TASK #1 //////////////////////////
+function revStrFunc(str) {
+  let strRev = "";
+  for (i = str.length - 1; i >= 0; i--) {
+    strRev += str[i];
+  }
+  console.log(strRev);
+}
+revStrFunc();
+////////////////////////// TASK #2 //////////////////////////
+const userList = [
+  {
+    userName: "Dan",
+    age: 16,
+    status: "active",
+  },
+  {
+    userName: "Mike",
+    age: 22,
+    status: "active",
+  },
+  {
+    userName: "Bob",
+    age: 40,
+    status: "active",
+  },
+];
+
+const findUser = userList.filter(
+  (el) => el.age >= 18 && el.status === "active"
+);
+
+for (obj of findUser) {
+  const { userName } = obj;
+  console.log(`${userName} is overgrown and active now`);
+}
